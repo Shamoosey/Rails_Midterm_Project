9123094f59d59056ccf7640e91e06b32e7d4a2b7
Shamus Osler <Shamuso23@gmail.com>
2020-02-20 16:57:06 -0600

    Created seed script for DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,53 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+require 'csv'
+require 'faker'
+
+Customer.destroy_all
+Beer.destroy_all
+Brewery.destroy_all
+
+breweries = File.read('/mnt/c/Users/Shamus/Documents/Rails_Midterm_Project/breweries.csv')
+beers = File.read('/mnt/c/Users/Shamus/Documents/Rails_Midterm_Project/beers.csv')
+
+csvBreweries = CSV.parse(breweries, headers: true)
+csvBeers = CSV.parse(beers, headers: true)
+
+NUMBER_OF_CUSTOMERS = 150
+
+csvBreweries.each do |brewery|
+  Brewery.create(
+    name: brewery['name'],
+    address: Faker::Address.street_address,
+    city: brewery['city'],
+    country: 'United States',
+    state: brewery['state']
+  )
+end
+
+csvBeers.each do |beer|
+  Beer.create(
+    name: beer['name'],
+    ibu: beer['ibu'],
+    abv: beer['abv'],
+    category: beer['style'],
+    brewery_id: beer['brewery_id']
+  )
+end
+
+NUMBER_OF_CUSTOMERS.times do
+  name = Faker::Name.name
+  Customer.create(
+    name: name,
+    address: Faker::Address.street_address,
+    city: Faker::Address.city,
+    state: Faker::Address.state,
+    country: 'United States',
+    email: Faker::Internet.email(name: name, separators: '_'),
+    beer_id: rand(1..Beer.count)
+  )
+end
+
+puts "Created #{Brewery.count} breweries"
+puts "Created #{Beer.count} beer"
+puts "Created #{Customer.count} customers"
