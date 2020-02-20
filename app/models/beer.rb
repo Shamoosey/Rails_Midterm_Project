ef7bd21bbd38a14a9467a07d02387be9c191716c
Shamus Osler <Shamuso23@gmail.com>
2020-02-20 17:08:49 -0600

    Added validations

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
 class Beer < ApplicationRecord
   belongs_to :brewery
+  validates :name, :brewery_id, presence: true
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 class Customer < ApplicationRecord
   belongs_to :beer
+  validates :name, :address, :country, :city, :state, presence: true
+  validates :email, uniqueness: true
 end
