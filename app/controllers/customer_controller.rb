dcbade1d2b2ab03d7ba5bc4e09dcb0dbf58b95ee
Shamus Osler <Shamuso23@gmail.com>
2020-02-28 16:51:28 -0600

    Added an index

--- app/controllers/customer_controller.rb
+++ app/controllers/customer_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class CustomerController < ApplicationController
+  def index
+    @customers = Customer.order(:name)
+  end
+
   def show
     @customer = Customer.find(params[:id])
   end
