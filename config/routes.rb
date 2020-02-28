7b4f20bfd65b58d48684b0ebb07962d541534cfc
Shamus Osler <Shamuso23@gmail.com>
2020-02-28 16:33:58 -0600

    Motified views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'beers/show'
+  resources :customer, only: :show
+  resources :beers, only: :show
   resources :breweries, only: :show
+
   root to: 'breweries#index'
 
   get 'breweries/:id', to: 'breweries#show', id: /\d+/
