8c2e3ef52d8764754fd65ad21b4baa110c4c4557
Shamus Osler <Shamuso23@gmail.com>
2020-02-28 15:02:44 -0600

    Revert "created views and controllers"
    This reverts commit 8dd219d24d572302f2a1fe543d76c93b2f635162.

--- app/controllers/customer_controller.rb
+++
@@ -1,7 +0,0 @@
-class CustomerController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  get 'customer/new'
-  get 'customer/create'
-  get 'breweries/new'
-  get 'breweries/create'
-  get 'beer/new'
-  get 'beer/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
