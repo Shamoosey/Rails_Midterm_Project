33071c8a6460d317d0f6c088b8b5dab5c016e63f
Shamus Osler <Shamuso23@gmail.com>
2020-02-28 16:51:45 -0600

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :customer, only: :show
-  resources :beers, only: :show
   resources :breweries, only: :show
+  resources :customer, only: :show
 
   root to: 'breweries#index'
 
   get 'breweries/:id', to: 'breweries#show', id: /\d+/
+  get 'customer', to: 'customer#index'
+  get 'customer/:id', to: 'customer#show', id: /\d+/
+  get 'beers/:id', to: 'beers#show', id: /\d+/
 end
