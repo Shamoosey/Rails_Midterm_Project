13000e2c76b3d8e0037c51fcd5fa3da65fa8bde0
Shamus Osler <Shamuso23@gmail.com>
2020-03-03 13:34:02 -0600

    Added search to controller

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 class BeersController < ApplicationController
   def show
     @beer = Beer.find(params[:id])
+  end
+
+  def search
+    @beers = Beer.where('category LIKE ?', "%#{params[:search_term]}%")
   end
 end
